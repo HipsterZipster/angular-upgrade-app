5273c14d330da6a3cf38eecedb7b092c6864539e
Joel Zipkin <yzipkin@gmail.com>
2016-05-05 11:18:32 -0600

    formatting

--- app/contact-editor-component/contact-editor-component.es5.js
+++ app/contact-editor-component/contact-editor-component.es5.js
@@ -1,36 +1,36 @@
 angular
-	.module('contact-editor-component', ['ngRoute', 'contacts-service', 'clone-service'])
-	.config(function ($routeProvider) {
-		$routeProvider.when('/contact/:id/edit', {
-			template: '<contact-editor-component><contact-editor-component>'
-		});
-	})
-	.component('contactEditorComponent', {
-		templateUrl: 'app/contact-editor-component/contact-editor-component.html',
-		controller: function (contactsService, cloneService, $location, $routeParams) {
+    .module('contact-editor-component', ['ngRoute', 'contacts-service', 'clone-service'])
+    .config(function ($routeProvider) {
+        $routeProvider.when('/contact/:id/edit', {
+            template: '<contact-editor-component><contact-editor-component>'
+        });
+    })
+    .component('contactEditorComponent', {
+        templateUrl: 'app/contact-editor-component/contact-editor-component.html',
+        controller: function (contactsService, cloneService, $location, $routeParams) {
 
-			this.contact = cloneService.createClone(contactsService.getContact($routeParams.id));
+            this.contact = cloneService.createClone(contactsService.getContact($routeParams.id));
 
-			//http://stackoverflow.com/questions/30537886/error-ngmodeldatefmt-expected-2015-05-29t190616-693209z-to-be-a-date-a
-			this.contact.birthday = new Date(this.contact.birthday);
+            //http://stackoverflow.com/questions/30537886/error-ngmodeldatefmt-expected-2015-05-29t190616-693209z-to-be-a-date-a
+            this.contact.birthday = new Date(this.contact.birthday);
 
-			this.$onInit = function () {
-				console.log('running');
-			};
+            this.$onInit = function () {
+                console.log('running');
+            };
 
-			this.cancel = function (contact) {
-				cloneService.abortChanges();
-				goToDetails(contact);
-			};
+            this.cancel = function (contact) {
+                cloneService.abortChanges();
+                goToDetails(contact);
+            };
 
-			this.save = function (contact) {
-				cloneService.commitChanges();
-				goToDetails(contact);
-			};
+            this.save = function (contact) {
+                cloneService.commitChanges();
+                goToDetails(contact);
+            };
 
-			goToDetails = function (contact) {
-				$location.url('/contact/' + contact.id);
-			};
+            goToDetails = function (contact) {
+                $location.url('/contact/' + contact.id);
+            };
 
-		}
-	});
+        }
+    });
